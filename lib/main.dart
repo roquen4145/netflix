8949f0ba02e6c1b1a6a79c659c512e52702e0f46
roquen4145 <roquen4145@gmail.com>
2022-08-31 22:54:08 +0900

    create homescreen topbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/screen/home_screen.dart';
 import 'package:netflix/widget/bottom_bar.dart';
 
 void main() {
@@ -13,37 +14,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // TabController controller;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'DdoFlix',
         theme: ThemeData(
-            brightness: Brightness.dark, primaryColor: Colors.black, accentColor: Colors.white
-        ),
-      home: DefaultTabController(
-          length: 4,
-          child: Scaffold(
-            body:TabBarView(
-              physics: NeverScrollableScrollPhysics(),
-              children: [
-                Container(
-                    child:Center(child: Text('home'))
-                ),
-                Container(
-                    child:Center(child: Text('search'))
-                ),
-                Container(
-                    child:Center(child: Text('save'))
-                ),
-                Container(
-                    child:Center(child: Text('more'))
-                )
-              ],
-            ),
+            brightness: Brightness.dark,
+            primaryColor: Colors.black,
+            accentColor: Colors.white),
+        home: DefaultTabController(
+            length: 4,
+            child: Scaffold(
+              body: TabBarView(
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  HomeScreen(),
+                  Container(child: Center(child: Text('search'))),
+                  Container(child: Center(child: Text('save'))),
+                  Container(child: Center(child: Text('more')))
+                ],
+              ),
               bottomNavigationBar: BottomBar(),
-          )
-      )
-    );
+            )));
   }
 }
