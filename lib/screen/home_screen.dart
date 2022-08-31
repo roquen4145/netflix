1928d940bdbde4b6fa43d60f71a62106fa2c9d7c
roquen4145 <roquen4145@gmail.com>
2022-09-01 00:10:38 +0900

    add movie model

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../model/model_movie.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -8,9 +10,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    })
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return TopBar();//Container(child: Center(child: Text('home')));
+    return TopBar(); //Container(child: Center(child: Text('home')));
   }
 }
 
@@ -30,26 +41,23 @@ class TopBar extends StatelessWidget {
             height: 25,
           ),
           Container(
-            padding: EdgeInsets.only(right: 1),
-            child: Text(
-              'TV 프로그램',
-              style: TextStyle(fontSize: 14),
-            )
-          ),
+              padding: EdgeInsets.only(right: 1),
+              child: Text(
+                'TV 프로그램',
+                style: TextStyle(fontSize: 14),
+              )),
           Container(
               padding: EdgeInsets.only(right: 1),
               child: Text(
                 '영화',
                 style: TextStyle(fontSize: 14),
-              )
-          ),
+              )),
           Container(
               padding: EdgeInsets.only(right: 1),
               child: Text(
                 '내가 찜한 콘텐츠',
                 style: TextStyle(fontSize: 14),
-              )
-          )
+              ))
         ],
       ),
     );
